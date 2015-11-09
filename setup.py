69b6293749cb86a3e93b1c1dd55ac0bf672ff7ff
Charles Hamilton <musashi@nefaria.com>
2015-11-09 12:02:21 -0500

    Added some info to setup.py/preparing to upload to pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from distutils.core import setup
 
+
 setup(
     name='django-clamav-upload',
     version='v0.1.0',
     packages=['clamav_upload','tests'],
-    url='https://github.com/musashiXXX/django-clamav-upload/releases/tag/v0.1.0',
+    url='https://github.com/musashiXXX/django-clamav-upload',
     license='LICENSE.txt',
     author='Charles Hamilton',
     author_email='musashi@nefaria.com',
     description='A simple upload handler for django that scans inbound streams for malicious content',
+    long_description='README.md',
     install_requires=[
         'Django >=1.8.4',
         'pyClamd >=0.3.15'
-    ]
+    ],
+    keywords='clamav django upload'
 )
